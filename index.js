a2f8284d96634aec23317f8c32db8a0f22152c80
Nikita Essine <nikita.essine@helsinki.fi>
2023-01-21 15:42:33 +0200

    3.1

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    id: 1,
+    content: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    content: "Ada Lovelace",
+    number: "39-44-5325252"
+  },
+  {
+    id: 3,
+    content: "Dan Abramov",
+    number: "12-34-4554545"
+  },
+  {
+    id: 4,
+    content: "Mary Poppendick",
+    number: "044-03803830"
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.post('/api/notes', (request, response) => {
+    const persons = request.body
+    console.log(persons)
+    response.json(persons)
+  }) 
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
